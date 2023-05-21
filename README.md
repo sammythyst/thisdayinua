8a89e8a199f5efab3dbb85bf8b1a8fffa98d67f8
Sam Swain <saswain96@gmail.com>
2023-05-20 23:36:07 -0500

    added thumbnails, starting json

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# thisdayinua
-A memorial page for the former YouTube channel, Unus Annus.
+# This Day In Unus Annus
+
+## Description 
+A memorial site for the now deleted YouTube channel, Unus Annus. This site displays the thumbnail and details from the video uploaded on the date read by your device.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,40 @@
-$(document).ready(function () {
-    var thisdayinua = moment().format("MMMM Do");
-    var display = document.getElementById('thisdayinua');
-    display.innerHTML = thisdayinua;
+let uaVideos = [];
+// displays current date on page
+var thisdayinua = moment().format("MMMM Do");
+var display = document.getElementById('thisdayinua');
+display.innerHTML = thisdayinua;
 
-    function anniversary() {
-        var thisDay = moment().format("MDD");
-        var date = $(".date");
-        
-            date.each(function() {
-                var calendarDate = $(this).attr('id');
-                console.log(calendarDate, thisDay);
 
-                if (calendarDate === thisDay) {
-                    $(this).addClass("today");
-                } else {
-                    $(this).addClass("nottoday");
-                }
-            })
-        }
+function fetchVideo() {
+    fetch("./assets/js/videos.json")
+        .then(response => response.json())
+        .then(data => listVideo(data));   
 
+        console.log(uaVideos.title)
+}
+
+function listVideo() {
+
+}
+
+
+// date in moment format MDD matches id
+function anniversary() {
+    var thisDay = moment().format("MDD");
+    var date = $(".date");
+    
+        date.each(function() {
+            var calendarDate = $(this).attr('id');
+            console.log(calendarDate, thisDay);
+
+            if (calendarDate === thisDay) {
+                $(this).addClass("today");
+            } else {
+                $(this).addClass("nottoday");
+            }
+        })
+    }
+
+fetchVideo();
+listVideo();
 anniversary();
-})
