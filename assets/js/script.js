fb37f598f8648618f30cf97f8b195692459a3fe2
sasw <saswain96@gmail.com>
2023-05-22 23:30:54 -0500

    some style, timer added

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,9 @@ fetch("./assets/js/videos.json")
 
                 if (calendarDate === thisDay) {
                     display += `
-                        <div class="today">
+                        <div>
+                            <h2 id="clock"></h2>
+                            <img src="${video.thumbnail}">
                             <h2>${video.title}</h2>
                             <p>${video.description}</p>
                         </div>
@@ -29,3 +31,23 @@ fetch("./assets/js/videos.json")
                 uaVideos.innerHTML = display;
         }
     })
+
+    // clock countdown
+    var countdownDate = new Date("November 14, 2023 00:00:00").getTime();
+
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countdownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("clock").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("clock").innerHTML = "00:00:00:00";
+        }
+    }, 1000);
