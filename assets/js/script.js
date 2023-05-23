a13f9ce20ef99f2302d106bb1dcadbba923ff3da
Sam Swain <saswain96@gmail.com>
2023-05-23 11:32:54 -0500

    clock displays min of 2 digits

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,11 @@ fetch("./assets/js/videos.json")
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+        if (days < "10") { days = "0" + days; }
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
         document.getElementById("clock").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
 
         if (distance < 0) {
