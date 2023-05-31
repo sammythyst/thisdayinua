bc2c05c482b885817a87d5feb482809cf055a117
Sam Swain <saswain96@gmail.com>
2023-05-31 15:11:46 -0500

    searchbar filters videos

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,29 @@
 const videoList = document.getElementById('videoList');
+const searchBar = document.getElementById('searchBar');
+let uaVideos = [];
 
+// search bar searches
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredVideos = uaVideos.filter(video => {
+        return video.title.toLowerCase().includes(searchString);
+    });
+    displayVideos(filteredVideos);
+});
+
+// calls for data in json
 const loadVideos = async () => {
     try {
         const res = await fetch("./assets/js/videos.json");
-        let uaVideos = await res.json();
+        uaVideos = await res.json();
         displayVideos(uaVideos);
     } catch (err) {
         console.error(err);
     }
 };
 
+// displays json data
 const displayVideos = (videos) => {
     const htmlString = videos
         .map((video) => {
