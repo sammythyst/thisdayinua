a7078137c1e36bbd2fc6835223befcc09b3e1452
sasw <saswain96@gmail.com>
2026-03-01 11:36:43 -0600

    removed simulated leap day 2026

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,8 @@ var x = setInterval(function() {
     var distance = countdownDate - now;
 
     // add +1 to var days to simulate leap year
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24) + 1);
-    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24) + 1);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
